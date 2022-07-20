3956413e4d67cb027b0521058d26b0a62b8d27d3
Wjs0068 <87343895+Wjs0068@users.noreply.github.com>
2022-07-19 18:31:18 -0700

    added some new tests for List

---
+++ src/components/List/test/Countries.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import Countries from "../Countries.js";
+
+describe("Countries Tests", () => {
+  it("initial state should have data loaded", () => {});
+});
